8f5c15485dea1b701801f7cd675224df26f52866
brianchemase <brianchemo@gmail.com>
2023-11-08 12:25:14 +0300

    upload data to the server

--- notification/sms.php
+++ notification/sms.php
@@ -1,60 +1,67 @@
 <?php
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-    // Handle the POST request with parameters
-    $phone = isset($_POST['phone']) ? $_POST['phone'] : null;
-    $message = isset($_POST['message']) ? $_POST['message'] : null;
+    // Read the JSON data from the request body
+    $json = file_get_contents('php://input');
+    $data = json_decode($json, true);
 
-    if ($phone && $message) {
-        // Your SMS sending logic here
-        // You can reuse the existing cURL code
-        $to = '254' . substr($phone, 1);
+    if ($data !== null) {
+        $phone = isset($data['phone']) ? $data['phone'] : null;
+        $message = isset($data['message']) ? $data['message'] : null;
 
-        $ApiKey = 'mrbZnidZL1aisGhRem5yQ38v1DjFZXdFamYpRr21YtQ=';
-        $ClientId = 'ae5e5440-3968-4422-8018-feb27cebd201';
-        $SenderId = 'SUCDIAGENCY';
-        $AccessKey = 'mwNYuBUe0fxbYqDyyYkz9gsgNIH5cP0H';
-        $curl = curl_init();
+        if ($phone && $message) {
+            // Your SMS sending logic here
+            // You can reuse the existing cURL code
+            $to = '254' . substr($phone, 1);
+            $ApiKey = 'mrbZnidZL1aisGhRem5yQ38v1DjFZXdFamYpRr21YtQ=';
+            $ClientId = 'ae5e5440-3968-4422-8018-feb27cebd201';
+            $SenderId = 'SUCDIAGENCY';
+            $AccessKey = 'mwNYuBUe0fxbYqDyyYkz9gsgNIH5cP0H';
+            $curl = curl_init();
 
-        curl_setopt_array($curl, array(
-            CURLOPT_URL => "https://api.onfonmedia.co.ke/v1/sms/SendBulkSMS",
-            CURLOPT_RETURNTRANSFER => true,
-            CURLOPT_ENCODING => "",
-            CURLOPT_MAXREDIRS => 10,
-            CURLOPT_TIMEOUT => 30,
-            CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
-            CURLOPT_CUSTOMREQUEST => "POST",
-            CURLOPT_POSTFIELDS => json_encode(array(
-                "SenderId" => $SenderId,
-                "MessageParameters" => array(
-                    array(
-                        "Number" => $to,
-                        "Text" => $message,
+            curl_setopt_array($curl, array(
+                CURLOPT_URL => "https://api.onfonmedia.co.ke/v1/sms/SendBulkSMS",
+                CURLOPT_RETURNTRANSFER => true,
+                CURLOPT_ENCODING => "",
+                CURLOPT_MAXREDIRS => 10,
+                CURLOPT_TIMEOUT => 30,
+                CURLOPT_HTTP_VERSION => CURL_HTTP_VERSION_1_1,
+                CURLOPT_CUSTOMREQUEST => "POST",
+                CURLOPT_POSTFIELDS => json_encode(array(
+                    "SenderId" => $SenderId,
+                    "MessageParameters" => array(
+                        array(
+                            "Number" => $to,
+                            "Text" => $message,
+                        ),
                     ),
+                    "ApiKey" => $ApiKey,
+                    "ClientId" => $ClientId,
+                )),
+                CURLOPT_HTTPHEADER => array(
+                    "accesskey: " . $AccessKey,
+                    "cache-control: no-cache",
+                    "content-type: application/json"
                 ),
-                "ApiKey" => $ApiKey,
-                "ClientId" => $ClientId,
-            )),
-            CURLOPT_HTTPHEADER => array(
-                "accesskey: " . $AccessKey,
-                "cache-control: no-cache",
-                "content-type: application/json"
-            ),
-        ));
+            ));
 
-        $response = curl_exec($curl);
-        $err = curl_error($curl);
-        curl_close($curl);
+            $response = curl_exec($curl);
+            $err = curl_error($curl);
+            curl_close($curl);
 
-        if (!$err) {
-            // Successfully sent the SMS
-            echo "SMS sent successfully.";
+            if (!$err) {
+                // Successfully sent the SMS
+                echo "SMS sent successfully.";
+            } else {
+                // Handle the cURL error
+                echo "Error: $err";
+            }
         } else {
-            // Handle the cURL error
-            echo "Error: $err";
+            // Missing phone or message parameters
+            echo "Missing phone or message parameters.";
         }
     } else {
-        // Missing phone or message parameters
-        echo "Missing phone or message parameters.";
+        // Invalid JSON data
+        echo "Invalid JSON data in the request body.";
     }
 } else {
     // Handle other HTTP methods (GET, PUT, DELETE, etc.)
